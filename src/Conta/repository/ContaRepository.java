18b501421ce0c6934837803815af87c1a92c54aa
ThiagoAndrill <thf_91@hotmail.com>
2023-02-23 16:37:26 -0300

    Task 6 e 7

---
+++ src/Conta/repository/ContaRepository.java
@@ -0,0 +1,17 @@
+package Conta.repository;
+
+public interface ContaRepository {
+	
+	//CRUD da conta
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar(Conta conta);
+	public void atualizar(Conta conta);
+	public void deletar(int numero);
+	
+	//Métodos Bancários
+	public void sacar(int numero, float valor);
+	public void depositar(int numero, float valor);
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
+
+}
